2ca527d6cd9e4792a39260e1fda2e92daefa43b3
NikolayGH <pokrnikol@gmail.com>
2019-07-31 21:18:48 +0500

    hometask_4 complete1

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,39 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.extensions.hideKeyboard
+import ru.skillbranch.devintensive.models.Bender
+
+class ProfileActivity : AppCompatActivity(){
+
+    lateinit var benderImage : ImageView
+    lateinit var textTxt : TextView
+    lateinit var messageEt : EditText
+    lateinit var sendBtn : ImageView
+
+    lateinit var benderObj : Bender
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        }
+
+
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+    }
+
+}
