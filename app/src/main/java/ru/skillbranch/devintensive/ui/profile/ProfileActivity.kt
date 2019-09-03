0cc4331206dcc377dc66873846e1347e67a895c2
NikolayGH <pokrnikol@gmail.com>
2019-09-03 08:48:58 +0500

    hometask_4 complete7

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -11,28 +11,60 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_profile_constraint.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
 
 class ProfileActivity : AppCompatActivity(){
+    companion object{
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
 
-    lateinit var benderImage : ImageView
-    lateinit var textTxt : TextView
-    lateinit var messageEt : EditText
-    lateinit var sendBtn : ImageView
-
-    lateinit var benderObj : Bender
+    var isEditMode = false
+    lateinit var viewFields : Map<String, TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_constraint)
-
+        initViews(savedInstanceState)
         }
 
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields  = mapOf(
+            "nickname" to tv_nick_name,
+            "rank" to tv_rank,
+            "firstName" to et_first_name,
+            "lastName" to et_last_name,
+            "about" to et_about,
+            "repository" to et_repository,
+            "rating" to tv_rating,
+            "respect" to tv_respect
+        )
+        btn_edit.setOnClickListener(View.OnClickListener {
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+        })
+    }
+
+    private fun showCurrentMode(isEdit: Boolean) {
+        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        for((_,v)in info){
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if(isEdit)255 else 0
+        }
+        ic_eye.visibility = if(isEdit) View.GONE else View.VISIBLE
+
+        wr_about.isCounterEnabled = isEdit
+
+    }
 
 
     override fun onSaveInstanceState(outState: Bundle?) {
+
         super.onSaveInstanceState(outState)
     }
 
