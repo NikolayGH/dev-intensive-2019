3590c64da2d64234dfd9bff3c1f4848d1d25ad20
NikolayGH <pokrnikol@gmail.com>
2019-09-05 21:10:28 +0500

    hometask_4 complete9

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,7 +1,9 @@
 package ru.skillbranch.devintensive.ui.profile
 
 import android.graphics.Color
+import android.graphics.ColorFilter
 import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,17 +12,22 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_profile_constraint.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 class ProfileActivity : AppCompatActivity(){
     companion object{
         const val IS_EDIT_MODE = "IS_EDIT_MODE"
     }
 
+    private lateinit var viewModel: ProfileViewModel
     var isEditMode = false
     lateinit var viewFields : Map<String, TextView>
 
@@ -28,11 +35,41 @@ class ProfileActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_constraint)
         initViews(savedInstanceState)
+        initViewModel()
+    }
+
+    private fun initViewModel(){
+        viewModel= ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel.getProfileData().observe(this, Observer { updateUI(it) })
+        viewModel.getTheme().observe(this, Observer { updateTheme(it) })
+    }
+
+    private fun updateTheme(mode: Int) {
+        delegate.setLocalNightMode(mode)
+    }
+
+    private fun updateUI(profile: Profile) {
+        profile.toMap().also {
+            for((k,v)in viewFields){
+                v.text = it[k].toString()
+            }
         }
+    }
+
+    private fun saveProfileInfo(){
+        Profile(
+            firstName = et_first_name.text.toString(),
+            lastName = et_last_name.text.toString(),
+            about= et_about.text.toString(),
+            repository = et_repository.text.toString()
+        ).apply {
+            viewModel.saveProfileData(this)
+        }
+    }
 
     private fun initViews(savedInstanceState: Bundle?) {
         viewFields  = mapOf(
-            "nickname" to tv_nick_name,
+            "nickName" to tv_nick_name,
             "rank" to tv_rank,
             "firstName" to et_first_name,
             "lastName" to et_last_name,
@@ -41,9 +78,17 @@ class ProfileActivity : AppCompatActivity(){
             "rating" to tv_rating,
             "respect" to tv_respect
         )
+        isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+        showCurrentMode(isEditMode)
+
         btn_edit.setOnClickListener(View.OnClickListener {
+            if(isEditMode) saveProfileInfo()
             isEditMode = !isEditMode
             showCurrentMode(isEditMode)
+        })
+
+        btn_switch_theme.setOnClickListener(View.OnClickListener {
+            viewModel.switchTheme()
         })
     }
 
@@ -57,15 +102,32 @@ class ProfileActivity : AppCompatActivity(){
             v.background.alpha = if(isEdit)255 else 0
         }
         ic_eye.visibility = if(isEdit) View.GONE else View.VISIBLE
-
         wr_about.isCounterEnabled = isEdit
 
+        with(btn_edit){
+            val filter: ColorFilter? = if(isEdit){
+                PorterDuffColorFilter(
+                    resources.getColor(R.color.color_accent, theme),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }else{
+                null
+            }
+
+            val icon = if(isEdit){
+                resources.getDrawable(R.drawable.ic_save_black_24dp, theme)
+            }else{
+                resources.getDrawable(R.drawable.ic_edit_black_24dp, theme)
+            }
+            background.colorFilter = filter
+            setImageDrawable(icon)
+        }
     }
 
 
     override fun onSaveInstanceState(outState: Bundle?) {
-
         super.onSaveInstanceState(outState)
+        outState?.putBoolean(IS_EDIT_MODE, isEditMode)
     }
 
 }
